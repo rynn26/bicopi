4033a0555500de61fdb67b1c2eadf0fae3905a3e
DhaniPutraa <dhani101004@gmail.com>
2025-05-04 15:34:50 +0700

    update login

--- bichopi_mobile/lib/profile.dart
+++ bichopi_mobile/lib/profile.dart
@@ -1,5 +1,8 @@
+import 'package:coba3/register.dart';
 import 'package:flutter/material.dart';
-import 'ubah_password.dart'; // Pastikan file ini ada
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'ubah_password.dart';
+import 'login.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -9,10 +12,79 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfilePage extends State<ProfileScreen> {
-  final TextEditingController _nameController =
-      TextEditingController(text: "John Doe");
-  final TextEditingController _emailController =
-      TextEditingController(text: "JohnDoe@gmail.com");
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  bool _isLoading = true; // To show a loading indicator
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    final user = Supabase.instance.client.auth.currentUser;
+    if (user != null) {
+      try {
+        final response = await Supabase.instance.client
+            .from('users')
+            .select()
+            .eq('id_user', user.id)
+            .single();
+
+        if (response.error != null) {
+          // Handle error
+          print('Error fetching user data: ${response.error!.message}');
+        } else {
+          setState(() {
+            _nameController.text = response['username'] ?? '';
+            _emailController.text = response['email'] ?? '';
+            _phoneController.text = response['phone'] ?? '';
+            _isLoading = false; // Data has been loaded
+          });
+        }
+      } catch (e) {
+        print('Error fetching user data: $e');
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    } else {
+      // Handle no user logged in
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _updateUserData() async {
+    final user = Supabase.instance.client.auth.currentUser;
+    if (user != null) {
+      final response = await Supabase.instance.client
+          .from('users')
+          .update({
+            'username': _nameController.text,
+            'email': _emailController.text,
+            'phone': _phoneController.text,
+          })
+          .eq('id_user', user.id);
+
+      if (response.error == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Data berhasil diperbarui")));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Error: ${response.error!.message}")));
+      }
+    }
+  }
+
+  Future<void> _logout() async {
+    await Supabase.instance.client.auth.signOut();
+    if (!mounted) return;
+    Navigator.of(context).pushReplacementNamed('/login');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,83 +96,84 @@ class _ProfilePage extends State<ProfileScreen> {
         ),
         centerTitle: true,
         backgroundColor: const Color(0xFF078603),
-        automaticallyImplyLeading: false, // Menghilangkan tombol back
+        automaticallyImplyLeading: false,
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Avatar
-            const Center(
-              child: CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage("assets/foto_profile.png"),
-              ),
-            ),
-            const SizedBox(height: 10),
-
-            // Nama & Email
-            Center(
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator()) // Show loading indicator
+          : SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    _nameController.text,
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
+                  const Center(
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage("assets/foto_profile.png"),
+                    ),
                   ),
-                  Text(
-                    _emailController.text,
-                    style: const TextStyle(fontSize: 14, color: Colors.grey),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          _nameController.text,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          _emailController.text,
+                          style:
+                              const TextStyle(fontSize: 14, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle("Informasi Akun"),
+                  _buildEditableTextField("Nama Lengkap", _nameController),
+                  _buildEditableTextField("Email", _emailController),
+                  _buildEditableTextField("No. Telepon", _phoneController),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle("Pengaturan Keamanan"),
+                  _buildClickableBox("Ubah Password", Icons.lock, () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const UbahPasswordScreen()),
+                    );
+                  }),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: _updateUserData,
+                      child: const Text("Simpan Perubahan",
+                          style: TextStyle(fontSize: 16)),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF078603),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: _logout,
+                      child:
+                          const Text("Logout", style: TextStyle(fontSize: 16)),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 20),
-
-            // Informasi Akun
-            _buildSectionTitle("Informasi Akun"),
-            _buildEditableTextField("Nama Lengkap", _nameController),
-            _buildEditableTextField("Email", _emailController),
-
-            const SizedBox(height: 20),
-
-            // Pengaturan keamanan
-            _buildSectionTitle("Pengaturan Keamanan"),
-            _buildClickableBox("Ubah Password", Icons.lock, () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const UbahPasswordScreen()),
-              );
-            }),
-
-            const SizedBox(height: 30),
-
-            // Tombol Logout
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.logout, color: Colors.white),
-                label: const Text("Logout",
-                    style: TextStyle(color: Colors.white, fontSize: 16)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF078603),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
-  // Widget untuk Judul Section
   Widget _buildSectionTitle(String title) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -111,13 +184,13 @@ class _ProfilePage extends State<ProfileScreen> {
     );
   }
 
-  // Widget untuk Input yang Bisa Diedit
   Widget _buildEditableTextField(
       String label, TextEditingController controller) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: TextField(
         controller: controller,
+        readOnly: true, // Supaya tidak bisa diubah manual oleh user
         decoration: InputDecoration(
           labelText: label,
           border: OutlineInputBorder(
@@ -128,7 +201,6 @@ class _ProfilePage extends State<ProfileScreen> {
     );
   }
 
-  // Widget untuk Kotak Klik "Ubah Password"
   Widget _buildClickableBox(String text, IconData icon, VoidCallback onTap) {
     return GestureDetector(
       onTap: onTap,
@@ -148,7 +220,8 @@ class _ProfilePage extends State<ProfileScreen> {
                 Text(text, style: const TextStyle(fontSize: 16)),
               ],
             ),
-            const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.black54),
+            const Icon(Icons.arrow_forward_ios,
+                size: 16, color: Colors.black54),
           ],
         ),
       ),
